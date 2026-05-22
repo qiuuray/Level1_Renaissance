5a5eff4fe01b47fa2bd3746a4b192683ada0fea0
rain-dimitrova <qiuuchu@gmail.com>
2026-05-22 14:50:44 +0200

    Fixed Spot Light
    Fixed the Spotlight following the mouse! Added title screen that leads to the start of the game

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void Game() 
+    {
+        SceneManager.LoadScene("Scene 1 Sample");
+    }
+}
